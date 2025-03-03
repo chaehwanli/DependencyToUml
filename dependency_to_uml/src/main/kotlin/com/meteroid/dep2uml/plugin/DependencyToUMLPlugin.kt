3fda5dc37b43fee7915e1e4f3c7793835c814ebc
chaehwanli <chaehwan.li@gmail.com>
2025-03-03 13:46:50 +0900

    add plugin and task - DependencyToUMLPlugin.kt - GenerateDependencyToUMLTask.kt - DependencyToUMLExtension.kt

---
+++ dependency_to_uml/src/main/kotlin/com/meteroid/dep2uml/plugin/DependencyToUMLPlugin.kt
@@ -0,0 +1,34 @@
+/*
+ *
+ *  * Copyright ${YEAR} Meteroid contributors
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *     http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package com.meteroid.dep2uml.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class DependencyToUMLPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        // 플러그인 설정
+        target.extensions.create("dependencyDiagram", DependencyToUMLExtension::class.java)
+
+        // 태스크 등록
+        target.tasks.register("generateDependencyDiagram", GenerateDependencyToUMLTask::class.java) {
+            // 태스크의 기본 설정을 여기에 추가할 수 있습니다.
+        }
+    }
+}
---
+++ dependency_to_uml/src/main/kotlin/com/meteroid/dep2uml/plugin/GenerateDependencyToUMLTask.kt
@@ -0,0 +1,40 @@
+/*
+ *
+ *  * Copyright ${YEAR} Meteroid contributors
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *     http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package com.meteroid.dep2uml.plugin
+
+import com.meteroid.dep2uml.analyzer.DefaultGradleDependencyAnalyzer
+import com.meteroid.dep2uml.generator.DefaultPlantUMLGenerator
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+class GenerateDependencyToUMLTask : DefaultTask() {
+    @TaskAction
+    fun generateDiagram() {
+        // 의존성 분석 및 다이어그램 생성 로직
+        val project = project
+        val analyzer = DefaultGradleDependencyAnalyzer()
+        val generator = DefaultPlantUMLGenerator()
+
+        // 의존성 분석
+        val dependencies = analyzer.analyzeProject(project)
+
+        // 다이어그램 생성
+        generator.generateDiagram(dependencies, "${project.buildDir}/dependency-diagram.puml")
+    }
+}
